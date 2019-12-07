a6623898218edec83b32ebb6a3eb52a363731d0d
Curtis Lee <curtisrlee98@gmail.com>
2019-12-06 20:20:47 -0800

    updated notebook

--- code/lexicon.py
+++ code/lexicon.py
@@ -1,2 +1,10 @@
 from lexicon_utils import *
 
+def preprocess(df):
+  '''
+  preprocess confessions to add needed data
+
+  :param df: confessions
+  :type df: pandas DataFrame
+  :return: DataFrame with more informations
+  '''
